78d3698e04fe7a709e5e8e631c3886bf7e190290
Chris Fischer <chris.fischer36@gmail.com>
2017-07-27 22:26:24 -0400

    Update background and popup

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,9 +1,115 @@
-'use strict';
+url_docs = [];
+current_tab_doc = null; // if null means new tab
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
-});
+function add_new_starting_node(doc) {
+    if (doc) {
+        url_docs.push(doc);
+    }
+}
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+function add_child(child) {
+    if (child) {
+        current_tab_doc['children'].push(child);
+    }
+}
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+chrome.tabs.onUpdated.addListener(function(updated_tab_id, changeInfo, tab) {
+    
+    new_url = changeInfo.url
+    if (!new_url || new_url == 'chrome://newtab/') {
+        return
+    }
+    if (current_tab_doc && current_tab_doc['parent']) {
+        parent_referenced = find_doc_in_branch(updated_tab_id, new_url, current_tab_doc['parent'])
+        if (parent_referenced) {
+            current_tab_doc = parent_referenced
+            return // its not null, there was a parent that we went back to, no need to add a new document
+        }
+    }
+
+    // parent id 
+    // is the parent new
+    if (!current_tab_doc) {
+        // create new document to store
+        doc = {
+            'id': updated_tab_id,
+            'url': new_url,
+            'children': [],
+            'parent': null,
+            'title': tab.title
+        }
+        add_new_starting_node(doc)
+        current_tab_doc = doc
+    } else {
+        // update appropriate document
+        doc = {
+            'id': updated_tab_id,
+            'url': new_url,
+            'children': [],
+            'parent': current_tab_doc,
+            'title': tab.title
+        }
+        add_child(doc)
+        current_tab_doc = doc
+    }
+    console.log(url_docs, current_tab_doc)
+}); 
+
+function find_doc_in_branch(target_id, target_url, base_doc) {
+    console.log('find one in branch called')
+    if (base_doc['id'] == target_id && base_doc['url'] == target_url) {
+        return base_doc
+    } else if (base_doc['parent']) {
+        return find_doc_in_branch(target_id, target_url, base_doc['parent'])
+    } else {
+        return null
+    }
+}
+
+function find_doc(target_id, target_url, arr_of_docs) {
+    console.log('find one called')
+    for (i = 0; i < arr_of_docs.length; i++) {
+        curr_doc = arr_of_docs[i]
+        if (curr_doc['id'] == target_id && curr_doc['url'] == target_url) {
+            return curr_doc
+        } else if (curr_doc['children'].length == 0) {
+            continue
+        } else {
+            return find_doc(target_id, curr_doc['children'])
+        }
+    }
+    return null
+}
+
+function on_activate_or_create(highlight_info) {
+
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function(tabs) {
+        var tab = tabs[0];
+        var id = tab.id;
+        var url = tab.url;
+
+        if (!id || !url) {
+            return;
+        }
+
+        if (url == 'chrome://newtab/') {
+            current_tab_doc = null
+        } else {
+            current_tab_doc = find_doc(id, url, url_docs)
+            if (!current_tab_doc) {
+                // is null, error
+            }
+        }
+        console.log(url_docs, current_tab_doc)
+    });
+}
+
+chrome.tabs.onActivated.addListener(on_activate_or_create)
+chrome.tabs.onCreated.addListener(on_activate_or_create)
+
+
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,50 @@
-'use strict';
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
 
-console.log('\'Allo \'Allo! Popup');
+
+document.addEventListener('DOMContentLoaded', function() {
+  div_id = document.getElementById('id');
+  chrome.storage.sync.get('current_tab', function(items) {
+    id = items['current_tab']['id'];
+    if (!id) {
+      return
+    }
+    div_id.textContent = id;
+  });
+
+  div_url = document.getElementById('urls');
+  chrome.storage.sync.get('urls', function(items) {
+    if (items['urls'] == null) {
+      return;
+    } 
+
+    for (i = 0; i < items['urls'].length; i++) {
+      var innerDiv = document.createElement('div')
+      div_url.appendChild(innerDiv);
+      innerDiv.innerHTML = items['urls'][i];
+    }
+  });
+});
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("started")
+  getCurrentTabUrl(function(url) {
+    console.log(url)
+    saveChanges(url, function(items) {
+      console.log(items)
+      // display items
+      div = document.getElementById('urls');
+      for (i = 0; i < items.length; i++) {
+        var innerDiv = document.createElement('div');
+
+        div.appendChild(innerDiv);
+        innerDiv.innerHTML = items[i];
+      }
+    });
+
+    
+  });
+});
+*/
